2e1c90f6aa48ee2e50cb5dd8476adb40b1c037d6
riyandika <riyandika.andhi@gmail.com>
2025-09-10 18:27:41 +0700

    add table follows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_072041) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_10_112532) do
+  create_table "follows", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followee_id"], name: "index_follows_on_followee_id"
+    t.index ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
+
   create_table "sleep_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "sleep_at", null: false
@@ -29,5 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_072041) do
     t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "follows", "users", column: "followee_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "sleep_records", "users"
 end
